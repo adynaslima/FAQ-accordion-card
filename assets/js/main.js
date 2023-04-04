4df9031e5c87ee5a1f0a47104763403e116e5be9
Adyna Lima <adynalsl@gmail.com>
2023-04-03 22:27:23 -0300

    feat: javascript

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const resposta = document.querySelectorAll(".tudo");
+resposta.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
